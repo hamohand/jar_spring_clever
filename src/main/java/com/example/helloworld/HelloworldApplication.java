1cecce4714a193b79c90a40024e11ebfc185ebb5
Muhend <hamohand@gmail.com>
2023-10-05 10:02:40 +0200

    modifs de départ

---
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -0,0 +1,30 @@
+package com.example.helloworld;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class HelloworldApplication {
+
+	@Value("${TARGET:Muhend}")
+	String target;
+
+	@RestController
+	class HelloworldController {
+		@GetMapping("/")
+		String hello() {
+			//return "Hello " + target + "!";
+			return "Hello " + target + "! Jar-CleverCloud, " + "le 05/10 9:00";
+		}
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+}
